ac455b81ecfcd15d86180d72f4e1f4b94fd2c9d2
frvgmxntx <frvgmxntx@proton.me>
2025-03-05 20:32:18 -0300

    bang bang snapshoted!

--- hypr/scripts/bkp-dots.sh
+++ hypr/scripts/bkp-dots.sh
@@ -6,6 +6,8 @@ cp -rf ~/.config/ignis ~/Projects/Git/dxts/
 cp -rf ~/.config/matugen ~/Projects/Git/dxts/
 cp -rf ~/.config/waybar ~/Projects/Git/dxts/
 
+cp -rf ~/.local/share/themes/FlatColor ~/Projects/Git/dxts/
+
 cd ~/Projects/Git/dxts/
 git add ./*
 git commit -m "bang bang snapshoted!"
