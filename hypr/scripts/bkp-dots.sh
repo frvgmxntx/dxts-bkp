bb6c8cf1946e2c8166630be089b0e8b34d936a64
frvgmxntx <frvgmxntx@proton.me>
2025-03-11 03:30:16 -0300

    bang bang snapshoted!

--- hypr/scripts/bkp-dots.sh
+++ hypr/scripts/bkp-dots.sh
@@ -2,11 +2,9 @@
 
 cp -rf ~/.config/fish ~/Projects/Git/dxts/
 cp -rf ~/.config/hypr ~/Projects/Git/dxts/
-cp -rf ~/.config/ignis ~/Projects/Git/dxts/
 cp -rf ~/.config/matugen ~/Projects/Git/dxts/
+cp -rf ~/.config/nvim ~/Projects/Git/dxts/
 cp -rf ~/.config/waybar ~/Projects/Git/dxts/
-
-cp -rf ~/.local/share/themes/FlatColor ~/Projects/Git/dxts/
 
 cd ~/Projects/Git/dxts/
 git add ./*
